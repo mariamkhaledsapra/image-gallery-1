ac5ab77e270425836033946f2727e10f7e0a0670
ayazeid <aya.zeid95@gmail.com>
2023-11-30 20:47:30 +0200

    Add boat image

--- init.cpp
+++ init.cpp
@@ -7,5 +7,8 @@ int main()
 {       
     Image ice_cream = Image ("ice_cream");
     ice_cream.display();
+    // cout<<"-------------"<<endl;
+    Image boat = Image ("boat");
+    boat.display();
     return 0;
 }
